095226704746c4fc615ca03a01827216a41049ed
Cyril Nussbaum <c.nussbaum@tinterra.ch>
2026-01-11 18:07:35 +0100

    .

--- src/Tinterra.Application/obj/Debug/net10.0/Tinterra.Application.AssemblyInfo.cs
+++ src/Tinterra.Application/obj/Debug/net10.0/Tinterra.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tinterra.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3f95287e701514911430b2ac2a86d57cbd70358")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3336aa136739214544a4c21294e32710873510f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tinterra.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tinterra.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Tinterra.Infrastructure.Identity/obj/Debug/net10.0/Tinterra.Infrastructure.Identity.AssemblyInfo.cs
+++ src/Tinterra.Infrastructure.Identity/obj/Debug/net10.0/Tinterra.Infrastructure.Identity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tinterra.Infrastructure.Identity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3f95287e701514911430b2ac2a86d57cbd70358")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3336aa136739214544a4c21294e32710873510f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tinterra.Infrastructure.Identity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tinterra.Infrastructure.Identity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
